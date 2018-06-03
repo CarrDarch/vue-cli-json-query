41db05381f39b0f2c3783c25a33601dd4b075ff5
Will Yelland-Chiswick <dev1@will-chiswick.co.uk>
2018-06-03 12:50:30 +0100

    showed wordpress api json

--- htdocs/src/components/NewsList.vue
+++ htdocs/src/components/NewsList.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="news-list">
+    <div v-if="results.length > 1" class="news-list">
+        <pre> {{output}} </pre>
         <div v-for="result in results" :key="result.id">
             <pre>{{ result }}</pre>
         </div>
@@ -11,7 +12,9 @@
 import axios from 'axios'
 
 const NYTBaseUrl = "https://api.nytimes.com/svc/topstories/v2/";
-const ApiKey = ".json?api-key=ade9e2bb7b8a46d08b3f315431baa2f5";
+const SeddonURL = "https://www.seddonhomes.co.uk/wp-json/wp/v2/pages/";
+const SeddonLocalURL = "http://dev.seddonredesign.local/wp-json/wp/v2/pages/";
+const ApiKey = "ade9e2bb7b8a46d08b3f315431baa2f5";
 
 function buildUrl (url) {
   return NYTBaseUrl + url + ".json?api-key=" + ApiKey
@@ -22,18 +25,37 @@ export default {
     data() {
         return {
             results: [],
+            errors: [],
             section: 'home'
         }
     },
     mounted () {
-        this.getPosts('home');
+        //this.getPosts('home')
+        this.wpGet(SeddonLocalURL)
+        //axios.get('http://dev.seddonredesign.local/wp-json/wp/v2/pages/15535')
+        //axios.get('http://dev.seddonredesign.local/wp-json/wp/v2/pages/15535')
+        //.then(response => {this.results = response.data.results})
     },
     methods: {
         getPosts(section) {
             let url = buildUrl(section);
-            axios.get('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=ade9e2bb7b8a46d08b3f315431baa2f5').then((response) => {
+            axios.get(url).then((response) => {
                 this.results = response.data.results;
-            }).catch( error => { console.log(error); });
+            }).catch(
+                error => { 
+                    this.errors = response.data.error;
+                    console.log(error); 
+            });
+        },
+        wpGet(url) {
+            
+            axios.get(url).then((response) => {
+                this.results = response.data;
+            }).catch(
+                error => { 
+                    this.errors = response.data.error;
+                    console.log(error); 
+            });
         }
     }
 }
